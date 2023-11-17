e7847295337317e63a3af7d3c2448baccee35d32
Nguyen The Quan <ntquan18102003@gmail.com>
2023-11-17 18:28:59 +0700

    add BUS layer

---
+++ LapTrinhTrucQuan-QuanLyChoThueNha/BUS/TraNhaBUS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DAO;
+
+namespace BUS
+{
+    public class TraNhaBUS
+    {
+        private TraNhaDAO traNhaDAO;
+        public TraNhaBUS()
+        {
+            traNhaDAO = new TraNhaDAO();
+        }
+        public List<TraNhaDTO> GetListTraNha()
+        {
+            List<TraNhaDTO> list = new List<TraNhaDTO>();
+            DataTable data = traNhaDAO.GetListTraNha();
+            foreach (DataRow item in data.Rows)
+            {
+                list.Add(new TraNhaDTO(item));
+            }
+
+            return list;
+        }
+        public bool InsertTraNha(string maSoThue, DateTime ngayTra, decimal tongTien)
+        {
+            return traNhaDAO.InsertTraNha(maSoThue, ngayTra, tongTien);
+        }
+        public bool UpdateTraNha(string maSoThue, DateTime ngayTra, decimal tongTien)
+        {
+            return traNhaDAO.UpdateTraNha(maSoThue, ngayTra, tongTien);
+        }
+        public bool DeleteTraNha(string maSoThue)
+        {
+            return traNhaDAO.DeleteTraNha(maSoThue);
+        }
+    }
+}
